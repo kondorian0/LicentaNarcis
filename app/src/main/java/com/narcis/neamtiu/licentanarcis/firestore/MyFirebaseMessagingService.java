73017a5aa934bf48a9f03d044af46f2e341aff35
narcis.neamtiu <narcis.neamtiu@devtelsoftware.com>
2021-05-04 12:48:23 +0300

    push notification local

---
+++ app/src/main/java/com/narcis/neamtiu/licentanarcis/firestore/MyFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package com.narcis.neamtiu.licentanarcis.firestore;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
---
+++ app/src/main/java/com/narcis/neamtiu/licentanarcis/firestore/NotificationBroadcastReceiver.java
@@ -0,0 +1,40 @@
+package com.narcis.neamtiu.licentanarcis.firestore;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
+import com.narcis.neamtiu.licentanarcis.R;
+
+public class NotificationBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String title = intent.getStringExtra("NOTIFICATION_TITLE");
+        String message = intent.getStringExtra("NOTIFICATION_MESSAGE");
+
+        // Create Notification Data
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context,"notifyNarcis")
+                .setSmallIcon(R.drawable.ic_play_icon)
+                .setContentTitle("Content Title")
+                .setContentText("Content Text")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(200, notificationBuilder.build());
+//
+//        // Init Worker
+//        val work = OneTimeWorkRequest.Builder(ScheduledWorker::class.java)
+//                .setInputData(notificationData)
+//                .build();
+//
+//        // Start Worker
+//        WorkManager.getInstance().beginWith(work).enqueue();
+    }
+}
